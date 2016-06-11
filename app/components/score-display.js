cf63cc0161819f9f70ac15757028a0ba14d100da
Alexis Córdova <acordova@mac.com>
2016-06-10 17:26:37 -0700

    Remove hard-coded bindings

--- app/components/score-display.js
+++ app/components/score-display.js
@@ -3,10 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'table',
   classNames: ['score'],
-  classNameBindings: ['isPreGame:pre-game', 'isInProgress:in-progress', 'isFinal:final'],
-  isPreGame: true,
-  isInProgress: false,
-  isFinal: false,
   attributeBindings: ['data-component'],
   'data-component': 'score'
 });
