d019625faaeae6242f0a032abfa915c50758b77d
Alexis Córdova <acordova@mac.com>
2016-06-10 17:25:14 -0700

    Add format-name helper to create first initial name

---
+++ app/helpers/format-name.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export function formatName(params, { firstName, lastName }) {
+  let firstInitial;
+
+  if (firstName.length <= 2) {
+    firstInitial = firstName;
+
+    return `${firstInitial} ${lastName}`;
+  } else {
+    firstInitial = firstName.charAt(0);
+
+    return `${firstInitial}. ${lastName}`;
+  }
+}
+
+export default Ember.Helper.helper(formatName);
---
+++ tests/unit/helpers/format-name-test.js
@@ -0,0 +1,10 @@
+import { formatName } from 'iibbsy/helpers/format-name';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format name');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatName([42]);
+  assert.ok(result);
+});
