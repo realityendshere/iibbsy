dc8e7833ca3f238ac47abd17c3f6531ca15e984e
Alexis Córdova <acordova@mac.com>
2016-06-11 09:32:06 -0700

    Add team logos to score-display component

--- app/models/scoreboard.js
+++ app/models/scoreboard.js
@@ -9,17 +9,27 @@ export default Model.extend({
   venue: DS.attr('string'),
   location: DS.attr('string'),
   gameStatus: DS.attr(),
+  isPreGame: Ember.computed('gameStatus', function() {
+    return this.get('gameStatus.status') === 'Pre-Game';
+  }),
   awayTeamName: DS.attr('string'),
   awayTeamAbbrev: DS.attr('string'),
+  awayTeamLogo: Ember.computed('awayTeamAbbrev', function() {
+    let awayTeamAbbrev = this.get('awayTeamAbbrev');
+
+    return `assets/images/team-logos/${awayTeamAbbrev.toLowerCase()}.png`;
+  }),
   awayTeamWins: DS.attr('number'),
   awayTeamLosses: DS.attr('number'),
   awayProbablePitcher: DS.attr(),
   homeTeamName: DS.attr('string'),
   homeTeamAbbrev: DS.attr('string'),
+  homeTeamLogo: Ember.computed('homeTeamAbbrev', function() {
+    let homeTeamAbbrev = this.get('homeTeamAbbrev');
+
+    return `assets/images/team-logos/${homeTeamAbbrev.toLowerCase()}.png`;
+  }),
   homeTeamWins: DS.attr('number'),
   homeTeamLosses: DS.attr('number'),
-  homeProbablePitcher: DS.attr(),
-  isPreGame: Ember.computed('gameStatus', function() {
-    return this.get('gameStatus.status') === 'Pre-Game';
-  })
+  homeProbablePitcher: DS.attr()
 });
