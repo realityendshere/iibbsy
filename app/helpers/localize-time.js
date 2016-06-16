d9aa08310b4b460a2733449b9b4727c008d58545
Alexis Córdova <acordova@mac.com>
2016-06-15 19:04:23 -0700

    Add localize-time helper

---
+++ app/helpers/localize-time.js
@@ -0,0 +1,44 @@
+import Ember from 'ember';
+import moment from 'moment';
+
+const timeZones = {
+  'ET': 'US/Eastern',
+  'CT': 'US/Central',
+  'MT': 'US/Mountain',
+  'PT': 'US/Pacific'
+};
+
+function convertTimeZone(zone) {
+  if (zone.length === 2) {
+    for (const tz in timeZones) {
+      return timeZones[zone];
+    }
+  }
+}
+
+export function localizeTime(gameDate, gameTime, ampm, gameTimeZone) {
+  let dateArr = gameDate.split('/'),
+    timeArr = gameTime.split(':'),
+    year = dateArr[0],
+    month = dateArr[1],
+    day = dateArr[2],
+    hour = parseFloat(timeArr[0]),
+    minutes = timeArr[1],
+    userTimeZone = moment.tz.guess(),
+    userTimeZoneAbbrev = moment.tz(userTimeZone).zoneAbbr(),
+    date,
+    timeZone,
+    localizedTime;
+
+    if ((ampm === 'PM') && (hour !== 12)) {
+      hour = hour + 12;
+    }
+
+    date = `${year}-${month}-${day} ${hour}:${minutes}`;
+    timeZone = convertTimeZone(gameTimeZone);
+    localizedTime = moment.tz(date, timeZone).tz(userTimeZone).format('h:mm');
+
+    return `${localizedTime} ${ampm} ${userTimeZoneAbbrev}`;
+}
+
+export default Ember.Helper.helper(localizeTime);
---
+++ tests/unit/helpers/localize-time-test.js
@@ -0,0 +1,10 @@
+import { localizeTime } from 'iibbsy/helpers/localize-time';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | localize time');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = localizeTime([42]);
+  assert.ok(result);
+});
