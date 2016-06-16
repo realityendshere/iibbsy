dffc4ba72aea40f92fdf1f84d8603394b1dbc970
Alexis Córdova <acordova@mac.com>
2016-06-15 17:38:10 -0700

    Add API polling

--- app/routes/scoreboard/index.js
+++ app/routes/scoreboard/index.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    let interval = 5000;
+
+    Ember.run.later(this, function() {
+      this.model().then((data) => {
+        this.controller.set('model', data);
+      });
+    }, interval);
+
     return this.store.findAll('scoreboard');
   }
 });
